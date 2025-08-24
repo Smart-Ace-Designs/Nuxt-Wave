91287b70b3b01afec58115e65fb5802f1e1a8ab0
Smart Ace <132539186+Smart-Ace-Designs@users.noreply.github.com>
2025-08-24 10:09:59 -0500

    Initial commit

---
+++ app/app.vue
@@ -0,0 +1,10 @@
+<template>
+  <NuxtRouteAnnouncer />
+  <UApp>
+    <main>
+      <!-- Remove <AppWelcome /> to start your project... -->
+      <AppWelcome />
+      <NuxtPage />
+    </main>
+  </UApp>
+</template>
---
+++ app/components/App/Welcome.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="flex h-screen flex-col items-center justify-center gap-6">
+    <h1 class="text-(--ui-primary) text-6xl font-bold">Nuxt App</h1>
+
+    <div class="flex w-80 gap-2">
+      <div class="flex-1">
+        <UButton
+          label="Documentation"
+          icon="i-lucide-square-play"
+          to="https://ui.nuxt.com/getting-started/installation/nuxt"
+          target="_blank"
+          class="flex w-full items-center justify-center"
+        />
+      </div>
+
+      <div class="flex-1">
+        <UButton
+          label="GitHub"
+          color="neutral"
+          variant="outline"
+          icon="i-simple-icons-github"
+          to="https://github.com/nuxt/ui"
+          target="_blank"
+          class="flex w-full items-center justify-center"
+        />
+      </div>
+    </div>
+
+    <UColorModeButton>
+      <template #fallback>
+        <UButton loading variant="ghost" color="neutral" />
+      </template>
+    </UColorModeButton>
+  </div>
+</template>
