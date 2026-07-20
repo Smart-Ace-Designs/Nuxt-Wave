e6ed0cfa11ec8397528f70a6c532e2281508835f
Smart Ace <132539186+Smart-Ace-Designs@users.noreply.github.com>
2026-07-20 12:25:41 -0500

    Adds local lucide icon package

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,11 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ["@nuxt/ui"],
   css: ["~/assets/css/main.css"],
+  icon: {
+    clientBundle: {
+      scan: true,
+    },
+  },
   app: {
     head: {
       htmlAttrs: {
